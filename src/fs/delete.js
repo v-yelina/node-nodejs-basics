0d7e895a3b076e96a5dabce2e0310e512bf7950c
v-yelina <heytridvaodin@gmail.com>
2022-11-30 11:06:36 +0100

    feat: finish task delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToRemovePath = path.join(__dirname, "/files/fileToRemove.txt");
+
 const remove = async () => {
-    // Write your code here 
+  fs.unlink(fileToRemovePath, (err) => {
+    if (err) throw new Error("FS operation failed");
+  });
 };
 
 await remove();
