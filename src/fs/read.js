60bae685b028f7bae5abbbb4b88dd5f8bf275fd6
v-yelina <heytridvaodin@gmail.com>
2022-11-30 15:17:08 +0100

    feat: finish read task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToReadPath = path.join(__dirname, "/files/fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  fs.readFile(fileToReadPath, "utf8", (err, data) => {
+    if (err) throw new Error("FS operation failed");
+    console.log(data);
+  });
 };
 
 await read();
