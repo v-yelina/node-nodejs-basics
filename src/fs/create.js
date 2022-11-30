74a41ddad1818c385d44b87101d38c05cac7fef9
v-yelina <heytridvaodin@gmail.com>
2022-11-30 10:26:43 +0100

    feat: finish task create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dir = path.join(__dirname, "/files/fresh.txt");
+const text = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  console.log(dir);
+  fs.access(dir, fs.constants.F_OK, (err) => {
+    if (err) {
+      fs.appendFile(dir, text, (err) => {});
+    } else {
+      throw new Error("FS operation failed");
+    }
+  });
 };
 
 await create();
