577d0cd2da1d05a0891418554eda354aa2ceb56a
v-yelina <heytridvaodin@gmail.com>
2022-12-01 13:06:17 +0100

    feat: finish list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const targetDir = path.join(__dirname, "/files");
+
 const list = async () => {
-    // Write your code here 
+  fs.readdir(targetDir, { withFileTypes: true }, (err, files) => {
+    if (err) throw new Error("FS operation failed");
+    else {
+      const filesNames = [];
+      files.forEach((file) => {
+        if (file.isFile()) {
+          filesNames.push(file.name);
+        }
+      });
+      console.log(filesNames);
+    }
+  });
 };
 
 await list();
