053da97355890dfab999ef2fe6fa40c7ba966d7f
v-yelina <heytridvaodin@gmail.com>
2022-12-02 15:39:54 +0100

    feat: finish writable stream task

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+import { createWriteStream } from "node:fs";
+import readline from "readline";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToWrite = path.join(__dirname, "/files/fileToWrite.txt");
+
 const write = async () => {
-    // Write your code here 
+  const writable = createWriteStream(fileToWrite);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false,
+  });
+
+  rl.on("line", (line) => {
+    writable.write(line + "\n");
+  }).on("close", () => {
+    writable.end();
+  });
 };
 
 await write();
