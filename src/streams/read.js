cdb86f2db1f0bb69d367077c00427c7e689a9191
v-yelina <heytridvaodin@gmail.com>
2022-12-02 15:29:15 +0100

    feat: finish reradable stream task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToRead = path.join(__dirname, "/files/fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  const readable = createReadStream(fileToRead);
+  readable.pipe(stdout);
 };
 
 await read();
