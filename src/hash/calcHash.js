bdf5a89bd4ff4de5350b83eff3b903e8d84acc65
v-yelina <heytridvaodin@gmail.com>
2022-12-02 15:24:20 +0100

    feat: finish hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+const { createHash } = await import("node:crypto");
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToHash = path.join(__dirname, "/files/fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const input = createReadStream(fileToHash);
+  const hash = createHash("sha256");
+  input.pipe(hash).setEncoding("hex").pipe(stdout);
 };
 
 await calculateHash();
