bc789e279121cacc6509378607f058f7731fd1b0
v-yelina <heytridvaodin@gmail.com>
2022-12-03 11:43:38 +0100

    feat: finish compress task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import { createGzip } from "node:zlib";
+import { getUrl } from "../getUrl.js";
+
 const compress = async () => {
-    // Write your code here 
+  const fileToCompressUrl = getUrl(import.meta.url, "files", "fileToCompress.txt");
+  const destinationUrl = getUrl(import.meta.url, "files", "archive.gz");
+  const readable = fs.createReadStream(fileToCompressUrl);
+  const writable = fs.createWriteStream(destinationUrl);
+  const gzip = createGzip();
+  readable
+    .pipe(gzip)
+    .pipe(writable)
+    .on("finish", () => {
+      writable.end();
+    });
 };
 
 await compress();
