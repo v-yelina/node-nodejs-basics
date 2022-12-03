8485ecc33470eb4dd77bfdbed7ef383ce6bcf0ef
v-yelina <heytridvaodin@gmail.com>
2022-12-03 11:43:47 +0100

    feat: finish decompress task

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import { createGunzip } from "node:zlib";
+import { getUrl } from "../getUrl.js";
+
 const decompress = async () => {
-    // Write your code here 
+  const fileToDecompressUrl = getUrl(import.meta.url, "files", "archive.gz");
+  const destinationUrl = getUrl(import.meta.url, "files", "fileToCompress.txt");
+  const readable = fs.createReadStream(fileToDecompressUrl);
+  const writable = fs.createWriteStream(destinationUrl);
+  const gzip = createGunzip();
+  readable
+    .pipe(gzip)
+    .pipe(writable)
+    .on("finish", () => {
+      writable.end();
+    });
 };
 
 await decompress();
