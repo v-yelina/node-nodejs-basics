0d89abeee8c6af607bd8695028d7a7567e0a7ac9
v-yelina <heytridvaodin@gmail.com>
2022-12-05 11:48:53 +0100

    feat: finish cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,12 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from "child_process";
+import { getUrl } from "../getUrl.js";
+
+const spawnChildProcess = async (...args) => {
+  const fileToSpawnUrl = getUrl(import.meta.url, "/files", "/script.js");
+  const spawnScript = spawn("node", [`${fileToSpawnUrl}`, ...args]);
+  spawnScript.stdout.on("data", (data) => {
+    console.log(data.toString());
+  });
 };
 
-spawnChildProcess();
+spawnChildProcess("arg1", "arg2", "arg3");
