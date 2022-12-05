efde0fb9b52a870d8fa72cf1011e74a4ded0908e
v-yelina <heytridvaodin@gmail.com>
2022-12-05 12:02:16 +0100

    fix: env result string

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,9 +1,11 @@
 const parseEnv = () => {
+  let resultStringArr = [];
   Object.entries(process.env).map((item) => {
     if (item[0].startsWith("RSS_")) {
-      console.log(`${item[0]}=${item[1]}`);
+      resultStringArr.push(`${item[0]}=${item[1]}`);
     }
   });
+  console.log(resultStringArr.join("; "));
 };
 
 parseEnv();
