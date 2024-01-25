28a64c254ada141682b5efa157e450ba6b96d67e
unknown <heytridvaodin@gmail.com>
2024-01-25 13:31:17 +0100

    feat: add error handling for hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,13 +1,17 @@
 import { createReadStream } from "node:fs";
 import { stdout } from "node:process";
+import path from "node:path";
 const { createHash } = await import("node:crypto");
-import { getUrl } from "../getUrl.js";
+import { argv } from "node:process";
 
-const fileToHash = getUrl(import.meta.url, "/files", "/fileToCalculateHashFor.txt");
+const fileToHash = `${path.dirname(argv[1])}${path.sep}files${path.sep}fileToCalculateHashFor.txt`;
 
 const calculateHash = async () => {
   const input = createReadStream(fileToHash);
   const hash = createHash("sha256");
+  input.on("error", (err) => {
+    process.stderr.write(err.message);
+  });
   input.pipe(hash).setEncoding("hex").pipe(stdout);
 };
 
