e26dff48588e55258d850dc1ba1c68e6d34e8832
unknown <heytridvaodin@gmail.com>
2024-01-25 12:59:55 +0100

    fix: files path in copy task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,28 +1,31 @@
 import fs from "fs";
 import path from "path";
-import { getUrl } from "../getUrl.js";
 
-const dirToCopy = getUrl(import.meta.url, "/files", "");
-const destinationDir = getUrl(import.meta.url, "/files_copy", "");
+const dirToCopy = `${path.dirname(process.argv[1])}${path.sep}files`;
+const destinationDir = `${path.dirname(process.argv[1])}${path.sep}files_copy`;
 
-const copy = async (source, destination) => {
-  fs.readdir(source, { withFileTypes: true }, (err, files) => {
+const copy = async () => {
+  fs.readdir(dirToCopy, { withFileTypes: true }, (err, files) => {
     if (err) throw new Error("FS operation failed");
     else {
-      fs.mkdir(destination, (err) => {
+      fs.mkdir(destinationDir, (err) => {
         if (err) throw new Error("FS operation failed");
       });
       files.forEach((file) => {
         if (file.isFile()) {
-          fs.copyFile(path.join(source, file.name), path.join(destination, file.name), (err) => {
-            if (err) throw new Error("FS operation failed");
-          });
+          fs.copyFile(
+            path.join(dirToCopy, file.name),
+            path.join(destinationDir, file.name),
+            (err) => {
+              if (err) throw new Error("FS operation failed");
+            }
+          );
         } else if (file.isDirectory()) {
-          copy(path.join(source, file.name), path.join(destination, file.name));
+          copy(path.join(dirToCopy, file.name), path.join(destinationDir, file.name));
         }
       });
     }
   });
 };
 
-copy(dirToCopy, destinationDir);
+await copy();
