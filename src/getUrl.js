0b3411ee6e9e6fd2a296d6292977d014927bc51b
unknown <heytridvaodin@gmail.com>
2024-01-26 10:24:39 +0100

    refactor: delete unused file

--- src/getUrl.js
+++
@@ -1,8 +0,0 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-export const getUrl = (url, folder, file) => {
-  const __filename = fileURLToPath(url);
-  const __dirname = path.dirname(__filename);
-  return path.join(__dirname, `${folder}${file}`);
-};
